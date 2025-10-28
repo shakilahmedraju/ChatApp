bebc8d272b68f85c1e20ebf28a409479bd64c967
shakilahmedraju <iamshakilahmedraju@gmail.com>
2025-10-28 14:33:09 +0600

    update db

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./chat.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./chat.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://chatapp_db_4u7d_user:OnOUd3A5HOabUNULe8TgB7fAGJlR7ph3@dpg-d407qm3e5dus7386p6gg-a:5432/chatapp_db_4u7d"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
